e24adf66e5cd58d32a9fc646b5bf352110323574
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-08-02 17:32:25 +0200

    Bootstrap cabal file

---
+++ write-yourself-a-scheme-lab.nix
@@ -0,0 +1,24 @@
+{ pkgs ? (import <nixpkgs> {})
+, haskellPackages ? pkgs.haskellPackages_ghc763
+}:
+
+let
+  inherit (haskellPackages) cabal cabalInstall_1_18_0_3 HUnit QuickCheck mtl; 
+
+in cabal.mkDerivation (self: {
+  pname = "write-yourself-a-scheme-lab";
+  version = "0.0.0.1";
+  sha256 = "dummy-sha1-for-the-moment";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  buildTools = [ cabalInstall_1_18_0_3 ];
+  buildDepends = with haskellPackages; [ mtl ];
+  testDepends = with haskellPackages; [ HUnit QuickCheck ];
+  meta = {
+    homepage = "https://github.com/ardumont/write-yourself-a-scheme-lab.git";
+    description = "Write yourself a scheme in Haskell";
+    license = self.stdenv.lib.licenses.gpl2;
+    platforms = self.ghc.meta.platforms;
+  };
+})
