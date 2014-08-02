6857cd13a85d4afc76d867dd5deee3e76eeadd1f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-08-02 18:36:46 +0200

    Refactor - Extract the arguments from CLI then dispatch to function to deal with display

--- Main.hs
+++ Main.hs
@@ -5,4 +5,7 @@ import System.Environment
 main :: IO ()
 main = do
   (arg0:arg1:_) <- getArgs
-  mapM_ putStr ["Hello, ", arg0, " ", arg1]
+  display arg0 arg1
+
+display :: String -> String -> IO ()
+display arg0 arg1 = mapM_ putStr ["Hello, ", arg0, " ", arg1]
